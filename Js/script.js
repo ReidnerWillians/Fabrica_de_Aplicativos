1f79c13f5befc4c474cd8298a8ebdcb50bafab79
Reidner Willians Pacheco Peres <pacheco195920@gmail.com>
2025-09-29 16:41:10 -0300

    js

--- Js/script.js
+++ Js/script.js
@@ -4,18 +4,20 @@ var altura;
 var imc;
 var resultado;
 
-function calcular(event) {
+function calcular(event){
     event.preventDefault();
 
-    peso = document.getElementById("peso").value;
-    altura = document.getElementById("altura").value;
+    peso = document.getElementById('peso').value;
+    altura = document.getElementById('altura').value;
+
+    console.log(peso);
+    console.log(altura);
 
     imc = peso / (altura * altura);
+    resultado = document.getElementById("resultado");
 
-    if(imc < 17){
-        resultado = document.getElementById("resultado");
-        resultado.innerHTML = "<br> Seu resultado foi: " + imc + "<br> Muito abaixo do peso";
-    }
-
-    console.log(resultado)
+    
+    if(imc < 17){        
+        resultado.innerHTML = "Seu resultado foi: " + imc + "<br> Muito abaixo do peso";
+    }    
 }
