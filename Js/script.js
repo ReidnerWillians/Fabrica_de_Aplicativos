cc874646656b04851517c77ea8f225a50a90c5da
Reidner Willians Pacheco Peres <pacheco195920@gmail.com>
2025-10-02 09:57:42 -0300

    js

--- Js/script.js
+++ Js/script.js
@@ -5,14 +5,27 @@ let buttonElement = document.querySelector('#app button');
 
 let tarefas = []
 
-function adicionarTarefa(){
-    if(inputElement.value === ''){
-         alert('Digite alguma coisa')
-        return false;
-    }else{
+function renderTarefas() {
+    listElement.innerHTML = '';
+
+    tarefas.map((todo) => {
+        let liElement = document.createElement('li');
+        let tarefaText = document.createTextNode(todo);
+
+        liElement.appendChild(tarefaText);
+        listElement.appendChild(liElement)
+    })
+}
+
+function adicionarTarefa() {
+    if (inputElement.value === '') {
+        alert('Digite alguma coisa')
+        return false; // Funções podem retornar false para sinalizar que uma operação não teve sucesso ou que uma determinada condição não foi satisfeita. 
+    } else {
         let novaTarefa = inputElement.value;
-        tarefas.push(novaTarefa);
-        inputElement.value = '';
+        tarefas.push(novaTarefa);//adicionando valores ao input
+        inputElement.value = '';//depois de adicionado, limpa o que foi adicionado
+        renderTarefas();
     }
 }
 buttonElement.onclick = adicionarTarefa;
